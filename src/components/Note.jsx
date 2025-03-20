0cf45532972de1fe8c5ea958bca51e2f9f6e529c
jabss <jabrijuhinin93@gmail.com>
2025-03-21 03:47:20 +0800

    trim time to only date

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,9 +2,11 @@ const Note = ({ note, toggleImportance }) => {
   const label = note.important
     ? 'make not important' : 'make important'
 
+  
+
   return (
     <li className='note'>
-      <span>{note.content}, {note.date} </span> 
+      <span>{note.content}, {note.date.split('T')[0]} </span> 
       <button onClick={toggleImportance}>{label}</button>
     </li>
   )
