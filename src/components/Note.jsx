5b531ab16aef42b2ea17c056d1d02552e233d215
jabss <jabrijuhinin93@gmail.com>
2025-03-21 03:52:11 +0800

    display date in format dd-mm-yyyy

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,11 +2,9 @@ const Note = ({ note, toggleImportance }) => {
   const label = note.important
     ? 'make not important' : 'make important'
 
-  
-
   return (
     <li className='note'>
-      <span>{note.content}, {note.date.split('T')[0]} </span> 
+      <span>{note.content}, {note.date.split('T')[0].split('-').reverse().join('-')} </span> 
       <button onClick={toggleImportance}>{label}</button>
     </li>
   )
